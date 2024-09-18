00ef80893847442295f1d4d2771b3ccaa31d7ed6
iothub336 <147072805+iothub336@users.noreply.github.com>
2024-09-18 13:47:26 +0530

    deleted cloudback folder

--- CloudBack/cloudapi/addarea/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from.models import AddArea
-admin.site.register(AddArea) 
--- CloudBack/cloudapi/addarea/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class AddArea(models.Model):
-    area=models.CharField(max_length=50,null=True,blank=True)
-   
-    def __str__(self) :
-        return self.area
